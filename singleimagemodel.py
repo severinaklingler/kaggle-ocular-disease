fe457644c951b761d84216f2792cdf75186ae4d0
Severin Klingler <severin.klingler@outlook.com>
2021-12-19 22:10:34 +0100

    Add initial data processing pipeline for single image model.

--- singleimagemodel.py
+++ singleimagemodel.py
@@ -18,7 +18,15 @@ from tensorflow.keras.utils import plot_model
 # Global config (TODO)
 random_seed = 77
 data_path = "./input/ocular-disease-recognition-odir5k/preprocessed_images/"
+data_path_tensor = tf.constant(data_path)
 data_dir = pathlib.Path(data_path)
+AUTOTUNE = tf.data.AUTOTUNE
+batch_size = 16
+img_height = 224
+img_width = 224
+class_count = 8
+image_channels = 3
+
 tf.config.run_functions_eagerly(True)
 
 
@@ -28,7 +36,7 @@ def load_sample_ids(df, val_size):
     dataset = tf.data.Dataset.from_tensor_slices(sample_ids)
     dataset = dataset.unique()
 
-    dataset = dataset.shuffle(100)
+    dataset = dataset.shuffle(len(sample_ids))
 
     val_ds = dataset.take(val_size)
     test_ds = dataset.skip(val_size).take(val_size)
@@ -38,12 +46,20 @@ def load_sample_ids(df, val_size):
 
 
 def build_label_dictionary(df):
-    dict = {}
+    keys = []
+    values = []
     for index, row in df.iterrows():
         filename = row['filename']
-        image_target = np.asarray(eval(row["target"]))
-        dict[filename] = image_target
-    return dict
+        target = eval(row["target"])
+        image_target = next(i for i,v in enumerate(target) if v==1)
+        keys.append(filename)
+        values.append(image_target)
+
+    keys_tensor = tf.constant(keys)
+    vals_tensor = tf.constant(values)
+    table = tf.lookup.StaticHashTable(tf.lookup.KeyValueTensorInitializer(keys_tensor, vals_tensor), default_value=-1)
+
+    return table
 
 def _extract_label(filename):
     return  label_dict[bytes.decode(filename.numpy())]
@@ -64,7 +80,7 @@ def has_label(filename):
 
 
 def _file_exists(file_path):
-    return tf.io.gfile.exists(file_path.numpy())
+    return tf.io.gfile.exists(data_path + bytes.decode(file_path.numpy()))
 
 def file_exists(file_path):
     [exists] = tf.py_function(_file_exists, [file_path], [tf.bool])
@@ -75,28 +91,90 @@ def file_exists(file_path):
 def filenames_from_id(id):
     right_path = tf.strings.as_string(id) + tf.constant("_right.jpg")
     left_path =  tf.strings.as_string(id) + tf.constant("_left.jpg")
+    return tf.data.Dataset.from_tensor_slices([left_path, right_path])
 
-    data = []
-    for p in [left_path, right_path]:
-        if file_exists(p):
-            if has_label(p):
-                img = p
-                data.append(img)
-    
-    return tf.data.Dataset.from_tensor_slices(data)
+def decode_img(img):
+    img = tf.io.decode_jpeg(img, channels=image_channels)
+    return tf.image.resize(img, [img_height, img_width])
 
 def process_filename(filename):
-    label = get_label(filename)
-    img = filename
-    return img, label
+    img = tf.io.read_file(tf.strings.join([data_path_tensor, filename], ''))
+    img = decode_img(img)
+    return img, label_dict.lookup(filename)
 
-def print_dataset_stats(names, datasets):
+def print_dataset_stats(names, datasets, n=-1):
     for name, dataset in zip(names, datasets):
+        if n>0:
+            dataset = dataset.take(n)
+
         d = list(dataset.as_numpy_iterator())
+        
+
         top5 = d[:5]
         print(f"{name} size: {len(d)} . First elements : {top5}")
 
+def label_not_missing(data, label):
+    return tf.math.not_equal(label,-1)
 
+def prepare_data(ds):
+    filenames = ds.flat_map(filenames_from_id)
+    existing_files = filenames.filter(file_exists)
+    existing_files_and_labels = existing_files.map(process_filename)
+    existing_files_and_existing_labels = existing_files_and_labels.filter(label_not_missing)
+    data_and_labels = existing_files_and_existing_labels.map(lambda x,y : (x, tf.one_hot(y,class_count)))
+    return data_and_labels
+
+def configure_for_performance(ds):
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+  return ds
+
+
+def create_model():
+    inp1 = Input(shape=(img_height,img_width,image_channels), name="img")
+    new_input = Input(shape=(img_height,img_width, image_channels), name="New Input")
+
+    conv1 = Conv2D(3, kernel_size=3, padding ='same', activation='relu', name="conleft1")(inp1)
+    i1 = tf.keras.applications.ResNet50(include_top=False,weights="imagenet",input_tensor=new_input,input_shape=None, pooling='avg')(conv1)
+
+    class1 = Dense(1024, activation='relu')(i1)
+    class1 = Dense(256, activation='relu')(class1)
+    class1 = Dense(64, activation='relu')(class1)
+    output = Dense(class_count, activation='sigmoid')(class1)
+    model = Model(inputs=[inp1], outputs=output)
+    return model
+
+def train_model(model, training_data, validation_data):
+    model = create_model()
+    METRICS = [
+        'accuracy',
+        tf.keras.metrics.Precision(),
+        tf.keras.metrics.Recall(),
+    ]  
+    model.compile(
+        optimizer='Adam',
+        loss='binary_crossentropy',
+        metrics=METRICS
+    )
+
+    tf.keras.utils.plot_model(
+        model,
+        to_file="model.png",
+        show_shapes=True,
+        show_dtype=False,
+        show_layer_names=True,
+        rankdir="TB",
+        expand_nested=False,
+        dpi=300,
+        layer_range=None,
+    )
+
+
+    model.fit(
+    training_data,
+    validation_data=validation_data,
+    epochs=50
+    )
 
 
 df = pd.read_csv('./input/ocular-disease-recognition-odir5k/full_df.csv')
@@ -104,9 +182,14 @@ label_dict = build_label_dictionary(df)
 
 
 train, val, test = load_sample_ids(df, 500)
-loaded_train = train.flat_map(filenames_from_id).map(process_filename)
+
+training_data = configure_for_performance(prepare_data(train))
+validation_data = configure_for_performance(prepare_data(val))
+test_data = configure_for_performance(prepare_data(test))
+print(training_data.element_spec)
+
+train_model(create_model(), training_data, validation_data)
 
 
-
-print_dataset_stats(["train", "val", "test", "loaded_train"],[train, val, test, loaded_train])
+# print_dataset_stats(["training_data"],[training_data],5)
 
